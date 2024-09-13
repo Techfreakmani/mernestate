811d766bf0713500c1520a419b6cf9de99d25e5e
Techfreakmani <manimani67784@gmail.com>
2024-09-13 11:11:35 +0530

    add redux toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react';
 import {Link} from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import {useDispatch,useSelector} from 'react-redux';
+import { signInStart,signInSuccess,signInFailure } from '../redux/user/userSlice';
 
 
 export default function SignIn() {
    
   const [formData,setFormData] = useState({});
-  const [error,setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  //const [error,setError] = useState(null);
+  //const [loading, setLoading] = useState(false); 
+  //use the above two lines as golbal using the useSelector
+  const { loading, error} = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
 
   const handleChange =(e)=>{
@@ -24,7 +29,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try{
-      setLoading(true);
+      dispatch(signInStart());
     //create a proxy to add localhost in front of fetch
     const res = await fetch('/api/auth/signin',
       {
@@ -39,18 +44,13 @@ export default function SignIn() {
     console.log(data);
     if(data.success === false)
     {
-      setLoading(false);
-      setError(data.message);
-      
-      
+      dispatch(signInFailure(data.message));
       return;
     }
-    setLoading(false);
-    setError(null);
+    dispatch(signInSuccess(data));
     navigate('/');
     }catch(error){
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));
     }
     
     
