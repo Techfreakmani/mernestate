1545e217f4dc61636bda376f665139fe75f3eb5b
Techfreakmani <manimani67784@gmail.com>
2024-09-28 08:31:13 +0530

    created update listing Api routes

--- api/controllers/lisiting.controller.js
+++ api/controllers/lisiting.controller.js
@@ -1,4 +1,7 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
+import mongoose from "mongoose";
+
 
 export const createListing = async (req, res, next) =>{
 
@@ -28,3 +31,37 @@ export const deleteListing = async (req, res, next) => {
       next(error);
     }
   };
+
+
+
+
+export const updateListing = async (req, res, next) => {
+  const { id } = req.params;
+
+  // Validate if the provided id is a valid ObjectId
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return next(errorHandler(400, 'Invalid Listing ID'));
+  }
+
+  try {
+    // Find the listing by its id
+    const listing = await Listing.findById(id);
+    if (!listing) {
+      return next(errorHandler(404, 'Listing not found!'));
+    }
+
+    // Check if the user is authorized to update this listing
+    if (req.user.id !== listing.userRef.toString()) {
+      return next(errorHandler(401, 'You can only update your own listings!'));
+    }
+
+    // Update the listing and return the updated version
+    const updatedListing = await Listing.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing,deleteListing } from '../controllers/lisiting.controller.js';
+import { createListing,deleteListing,updateListing } from '../controllers/lisiting.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -8,6 +8,6 @@ const router = express.Router();
 
 router.post('/create' , verifyToken,createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
-
+router.post('/update/:id', verifyToken, updateListing);
 
 export default router;
